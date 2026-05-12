04b85322f85a70a5757c30a0a32c37900c0afe06
sudhanshunitinatalkar <atalkarsudhanshu@proton.me>
2026-05-12 19:34:29 +0530

    update

--- modules/dolibarr.nix
+++ modules/dolibarr.nix
@@ -4,17 +4,16 @@ let
     services.cloudflared = {
       enable = true;
       tunnels = {
-        # Using the same UUID from your example
         "2815841f-6d4d-4fb5-adf0-5c82ecab3238" = {
           credentialsFile = "/home/sudha/.cloudflared/2815841f-6d4d-4fb5-adf0-5c82ecab3238.json";
           
           ingress = {
-            # Route the ERP domain to the internal Nginx port
             "erp.protoplast.in" = "http://localhost:8002";
-            
-            # Cloudflared REQUIRES a catch-all rule at the end, or the service will fail to start
-            "*" = "http_status:404";
+            # (Remove the "*" = "http_status:404"; from here if you still have it)
           };
+          
+          # PUT THIS BACK HERE: The dedicated NixOS fallback rule
+          default = "http_status:404";
         };
       };
     };
