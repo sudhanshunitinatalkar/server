ed6f4fb7676287680523d2caf72d156c2ac1f39d
sudhanshunitinatalkar <atalkarsudhanshu@proton.me>
2026-05-12 19:13:21 +0530

    update

--- modules/filebrowser.nix
+++ modules/filebrowser.nix
@@ -5,13 +5,13 @@ let
       enable = true;
       tunnels = {
         # Replace this UUID with your actual tunnel UUID
-        "19ff26dd-5675-4706-bd45-fc8772bcc64e" = {
+        "7513def7-e0e2-400d-b63e-7e01ee6e3938" = {
           # Make sure the credentials file exists at this path and is readable by the cloudflared service
-          credentialsFile = "/home/sudha/.cloudflared/19ff26dd-5675-4706-bd45-fc8772bcc64e.json";
+          credentialsFile = "/home/sudha/.cloudflared/7513def7-e0e2-400d-b63e-7e01ee6e3938.json";
           
           ingress = {
             # Replace "files.yourdomain.com" with your actual Cloudflare domain route
-            "server.protoplast.in" = "http://localhost:8001";
+            "files.protoplast.in" = "http://localhost:8001";
           };
           
           # Fallback rule if the hostname doesn't match
